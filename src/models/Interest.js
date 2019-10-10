b505aae8930e88ce0521a18fba4f49e88555bc23
Marcos TGA <marcos.adamczuk@gmail.com>
2019-10-09 22:00:19 -0300

    removed linter errors in models/Interest.js

--- src/models/Interest.js
+++ src/models/Interest.js
@@ -1,15 +1,14 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Interest = sequelize.define('Interest', {
     name: DataTypes.STRING,
     description: DataTypes.STRING,
-    image_path: DataTypes.STRING
+    image_path: DataTypes.STRING,
   }, {
     underscored: true,
     tableName: 'interest',
   });
-  Interest.associate = function(models) {
-    Interest.belongsToMany(models.User, {as: 'user', through: 'user_interest', foreignKey: 'interest_id'})
+  Interest.associate = (models) => {
+    Interest.belongsToMany(models.User, { as: 'user', through: 'user_interest', foreignKey: 'interest_id' });
   };
   return Interest;
 };
