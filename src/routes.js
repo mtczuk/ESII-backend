e9c6e36a657629e2f992b47bfc095e7e93ccc8c2
Marcos TGA <marcos.adamczuk@gmail.com>
2019-10-15 15:59:37 -0300

    fixed linter error

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const routes = express.Router();
-const authMiddleware = require('./middlewares/auth.js');
+// const authMiddleware = require('./middlewares/auth.js');
 const userController = require('./controllers/userController.js');
 const authenticationController = require('./controllers/authenticationController');
 // const interestController = require('./controllers/interestController.js');
