c3f441bcc2a6a4d753774965a73812566f79a4b4
Marcos TGA <marcos.adamczuk@gmail.com>
2019-10-15 21:18:23 -0300

    fixed linter errors

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,30 +1,30 @@
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 const { status, sendError } = require('../status');
-const jwt = require("jsonwebtoken");
 
 const secret = 'testsecret'; // must be changed to process.env.SECRET later
 
 module.exports = {
-    async authenticate(request, response){
-        const { email, password } = request.body;
+  async authenticate(request, response) {
+    const { email, password } = request.body;
 
-        try {
-            const user = await User.findOne({ where: {email, password}});
+    try {
+      const user = await User.findOne({ where: { email, password } });
 
-            if(!user) {
-                return sendError(response, status.USER_DOES_NOT_EXIST);
-            }
+      if (!user) {
+        return sendError(response, status.USER_DOES_NOT_EXIST);
+      }
 
-            const { id } = user;
+      const { id } = user;
 
-            token = await jwt.sign({ id }, secret);
+      const token = await jwt.sign({ id }, secret);
 
-            return response.json({
-                user,
-                token
-            })
-        } catch (err) {
-            return sendError(response, status.WRONG_PASSWORD);
-        }
+      return response.json({
+        user,
+        token,
+      });
+    } catch (err) {
+      return sendError(response, status.WRONG_PASSWORD);
     }
-}
+  },
+};
