6094d4129a12aba6f832dfd4eb4dbe0580235012
Marcos TGA <marcos.adamczuk@gmail.com>
2019-10-16 21:44:26 -0300

    changed test file structure

---
+++ server.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+// disabled because of mocha
+
+const { expect } = require('chai');
+const request = require('request');
+const { status } = require('../src/status');
+
+const baseUrl = 'http://127.0.0.1:8080/api';
+
+it('invalid route', (done) => {
+  request(`${baseUrl}/llaskjdf`, (err, res, body) => {
+    const { httpStatus, apiStatus } = status.INVALID_ROUTE;
+    const bodyObj = JSON.parse(body);
+
+    expect(res.statusCode).to.equal(httpStatus);
+    expect(bodyObj.httpStatus).to.equal(httpStatus);
+    expect(bodyObj.apiStatus).to.equal(apiStatus);
+
+    done();
+  });
+});
