de650be882a9f206619398b71c850ff0d092a65e
aluno-uffs <aluno-uffs@lab-cc.cco.uffs.edu.br>
2019-11-26 17:25:42 -0300

    added dev and prod options to database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,23 @@
 // configurações do ambiente de desenvolvimento
 module.exports = {
-  username: 'postgres',
-  password: 'postgres',
-  database: 'helippa_teste',
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-  ssl: true,
+  development: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'helippa_teste',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    define: {
+      timestamps: false,
+    },
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    port: 5432,
+    dialect: 'postgres',
+    ssl: true,
+  },
 };
