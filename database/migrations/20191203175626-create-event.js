0cc51ff4616f7de542d0a0f4d0ed500f1ef054e7
Marcos TGA <marcos.adamczuk@gmail.com>
2019-12-03 15:00:56 -0300

    created Event model

---
+++ database/migrations/20191203175626-create-event.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      place: {
+        type: Sequelize.STRING
+      },
+      picture: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Events');
+  }
+};
---
+++ src/models/Event.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Event = sequelize.define('Event', {
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    date: DataTypes.DATE,
+    place: DataTypes.STRING,
+    picture: DataTypes.STRING
+  }, {});
+  Event.associate = function(models) {
+    // associations can be defined here
+  };
+  return Event;
+};
