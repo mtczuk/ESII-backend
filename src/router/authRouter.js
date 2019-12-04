c9588a6cd0ea0b7a1e4cdfa195d8c7747ffd2291
Marcos TGA <marcos.adamczuk@gmail.com>
2019-12-04 13:07:45 -0300

    refacotred auth controller into auth router

---
+++ src/router/authRouter.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const { User } = require('../models');
+const { status, sendError } = require('../status');
+require('dotenv').config();
+
+const router = express.Router();
+const secret = 'testsecret'; // must be changed to process.env.SECRET later
+
+router.post('/', async (request, response) => {
+  const fbToken = request.headers.fbtoken;
+
+  if (fbToken) {
+    let data;
+    try {
+      const res = await axios.get('https://graph.facebook.com/v5.0/me', {
+        params: {
+          access_token: fbToken,
+          fields: 'name,email',
+        },
+      });
+      data = res.data;
+    } catch (e) {
+      return sendError(response, status.BAD_REQUEST);
+    }
+
+    if (!data.email) {
+      sendError(response, status.BAD_REQUEST);
+    }
+
+    let user;
+    try {
+      user = await User.findOne({ where: { email: data.email } });
+      if (user === null) {
+        user = await User.create({
+          name: data.name,
+          email: data.email,
+          password: '123',
+          // password está hardcoded pois não consegui remover a coluna nas migrations
+        });
+      }
+    } catch (e) {
+      return sendError(response, status.SERVER_ERROR);
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.SECRET || secret);
+
+    return response.json({ token });
+  }
+
+  return sendError(response, status.BAD_REQUEST);
+});
+
+module.exports = router;
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 
-// const passport = require('./passport');
-
 const routes = express.Router();
 const authMiddleware = require('../middlewares/auth');
-const authenticationController = require('../controllers/authenticationController');
 
 const userRouter = require('./userRouter');
+const authRouter = require('./authRouter');
 
-routes.post('/authenticate', authenticationController.authenticate);
-
+routes.use('/authenticate', authRouter);
 routes.use(authMiddleware);
 routes.use('/user', userRouter);
 
